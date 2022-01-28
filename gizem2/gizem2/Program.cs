0090a20dc877ed54961f7d4f0be6f1b0ae31ec5c
Alparslan Tekkanat <tekkanatalp@gmail.com>
2022-01-29 01:27:20 +0300

    otopark

---
+++ gizem2/gizem2/Program.cs
@@ -0,0 +1,171 @@
+﻿
+
+using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        int aracTipi = 0;
+        bool result = true;
+        double odenecekTutar = 0;
+        double kalinanSure = 0;
+
+
+
+        Console.WriteLine("1. Otomobil için 1'e basın \n" +
+        "2. Motosiklet için 2'ye basın\n" +
+        "3. Minibüs için 3'e basın\n" +
+        "4. Kamyon (ve diğer ticari araçlar) için 4'e basın\n");
+
+        if (int.TryParse(Console.ReadLine(), out aracTipi))
+        {
+            Console.Write("Kalınan Süreyi Giriniz : ");
+            kalinanSure = Convert.ToDouble(Console.ReadLine());
+
+            if (kalinanSure >= 0 && kalinanSure <= 2)
+            {
+                for (double i = 0; i <= kalinanSure; i += 0.1)
+                {
+                    if (i == 0)
+                    {
+                        if (aracTipi == 1)
+                        {
+                            odenecekTutar = 5;
+                        }
+                        else if (aracTipi == 2)
+                        {
+                            odenecekTutar = 0;
+                        }
+                        else if (aracTipi == 3)
+                        {
+                            odenecekTutar = 8;
+                        }
+                        else if (aracTipi == 4)
+                        {
+                            odenecekTutar = 15;
+                        }
+                    }
+                }
+            }
+            else if (kalinanSure > 2 && kalinanSure <= 6)
+            {
+                for (double i = 2.1; i < kalinanSure; i += 0.1)
+                {
+                    if (i == 2.1)
+                    {
+                        if (aracTipi == 1)
+                        {
+                            odenecekTutar = 10;
+                        }
+                        else if (aracTipi == 2)
+                        {
+                            odenecekTutar = 6;
+                        }
+                        else if (aracTipi == 3)
+                        {
+                            odenecekTutar = 16;
+                        }
+                        else if (aracTipi == 4)
+                        {
+                            odenecekTutar = 30;
+                        }
+                    }
+                }
+            }
+            else if (kalinanSure > 6 && kalinanSure <= 12)
+            {
+                for (double i = 6.1; i < kalinanSure; i += 0.1)
+                {
+                    if (i == 6.1)
+                    {
+                        if (aracTipi == 1)
+                        {
+                            odenecekTutar = 20;
+                        }
+                        else if (aracTipi == 2)
+                        {
+                            odenecekTutar = 12;
+                        }
+                        else if (aracTipi == 3)
+                        {
+                            odenecekTutar = 32;
+                        }
+                        else if (aracTipi == 4)
+                        {
+                            odenecekTutar = 60;
+                        }
+                    }
+                }
+            }
+            else if (kalinanSure > 12 && kalinanSure <= 24)
+            {
+                for (double i = 12.1; i < kalinanSure; i += 0.1)
+                {
+                    if (i == 12.1)
+                    {
+                        if (aracTipi == 1)
+                        {
+                            odenecekTutar = 35;
+                        }
+                        else if (aracTipi == 2)
+                        {
+                            odenecekTutar = 20;
+                        }
+                        else if (aracTipi == 3)
+                        {
+                            odenecekTutar = 45;
+                        }
+                        else if (aracTipi == 4)
+                        {
+                            odenecekTutar = 100;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (double i = 24.1; i < kalinanSure; i += 0.1)
+                {
+                    if (i == 24.1)
+                    {
+                     
+                            if (aracTipi == 1)
+                            {
+                                odenecekTutar = 55;
+                            }
+                            else if (aracTipi == 2)
+                            {
+                                odenecekTutar = 30;
+                            }
+                            else if (aracTipi == 3)
+                            {
+                                odenecekTutar = 70;
+                            }
+                            else if (aracTipi == 4)
+                            {
+                                odenecekTutar = 155;
+                            }
+                        
+                    }
+                }
+            }
+
+
+
+        }
+
+        Console.WriteLine("Ödenecek Tutar : {0} TL", odenecekTutar);
+        
+        
+
+
+
+
+
+
+    }
+    
+
+}
